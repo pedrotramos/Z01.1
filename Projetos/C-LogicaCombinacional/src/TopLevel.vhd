7bba8c21799167b559cd377ce62865b827a73f8f
Pellizzon <matheuspellizzon@hotmail.com>
2019-03-07 17:20:49 -0300

    Rubrica C

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -20,6 +20,9 @@ use work.all;
 entity TopLevel is
 	port(
 		SW      : in  std_logic_vector(9 downto 0);
+		HEX0    : out std_logic_vector(6 downto 0); -- 7seg0
+		HEX1    : out std_logic_vector(6 downto 0); -- 7seg1
+		HEX2    : out std_logic_vector(6 downto 0); -- 7seg2
 		LEDR    : out std_logic_vector(9 downto 0)
 	);
 end entity;
@@ -38,6 +41,24 @@ architecture rtl of TopLevel is
 ---------------
 begin
  
-LEDR(0) <= SW(0);
+	HEX0 <= "1000000" when SW(0) = '0' and SW(1) = '0' and SW(2) = '0' and SW(3) = '0' else --0
+			  "1111001" when SW(0) = '1' and SW(1) = '0' and SW(2) = '0' and SW(3) = '0' else --1
+			  "0100100" when SW(0) = '0' and SW(1) = '1' and SW(2) = '0' and SW(3) = '0' else --2
+			  "0110000" when SW(0) = '1' and SW(1) = '1' and SW(2) = '0' and SW(3) = '0' else --3
+			  "0011001" when SW(0) = '0' and SW(1) = '0' and SW(2) = '1' and SW(3) = '0' else --4
+			  "0010010" when SW(0) = '1' and SW(1) = '0' and SW(2) = '1' and SW(3) = '0' else --5
+			  "0000010" when SW(0) = '0' and SW(1) = '1' and SW(2) = '1' and SW(3) = '0' else --6
+			  "1111000" when SW(0) = '1' and SW(1) = '1' and SW(2) = '1' and SW(3) = '0' else --7
+			  "0000000" when SW(0) = '0' and SW(1) = '0' and SW(2) = '0' and SW(3) = '1' else --8
+			  "0011000" when SW(0) = '1' and SW(1) = '0' and SW(2) = '0' and SW(3) = '1' else --9
+			  "0001000" when SW(0) = '0' and SW(1) = '1' and SW(2) = '0' and SW(3) = '1' else --A
+			  "0000011" when SW(0) = '1' and SW(1) = '1' and SW(2) = '0' and SW(3) = '1' else --B
+			  "1000110" when SW(0) = '0' and SW(1) = '0' and SW(2) = '1' and SW(3) = '1' else --C
+			  "0100001" when SW(0) = '1' and SW(1) = '0' and SW(2) = '1' and SW(3) = '1' else --D
+			  "0000110" when SW(0) = '0' and SW(1) = '1' and SW(2) = '1' and SW(3) = '1' else --E
+			  "0001110" when SW(0) = '1' and SW(1) = '1' and SW(2) = '1' and SW(3) = '1' else --F
+			  "0111111";
+	
+	
 
 end rtl;
