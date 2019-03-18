926667a161563aa143c16e4bf424da55998bb49a
Pellizzon <matheuspellizzon@hotmail.com>
2019-03-17 21:36:17 -0300

    ULA e TopLevel feitos

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/ALU.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/ALU.vhd
@@ -25,6 +25,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.all;
+
 
 entity ALU is
 	port (
@@ -97,5 +99,22 @@ architecture  rtl OF alu is
 
 begin
   -- Implementação vem aqui!
+
+  ZeradorX: zerador16 port map (z=>zx, a=>x, y=>zxout);
+  ZeradorY: zerador16 port map (z=>zy, a=>y, y=>zyout);
+
+  IversorX: inversor16 port map (z=>nx, a=>zxout, y=>nxout);
+  IversorY: inversor16 port map (z=>ny, a=>zyout, y=>nyout);
+
+  Mult: And16 port map (a=>nxout, b=>nyout, q=>andout);
+  Adder: Add16 port map (a=>nxout, b=>nyout, q=>adderout);
+
+  Mux: Mux16 port map (a=>andout, b=>adderout, sel=>f, q=>muxout);
+
+  Precomparador: inversor16 port map (z=>no, a=>muxout, y=>precomp);
+
+  Comparador: comparador16 port map (a=>precomp, zr=>zr, ng=>ng);
+
+  saida <= precomp;
 
 end architecture;
